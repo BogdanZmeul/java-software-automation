16da7009b9a68b5b2614df82833fd68fe6a7e4d3
bogdanzmeul <bogdanzmeul@gmail.com>
2026-05-19 16:21:44 +0300

    Update system characteristics to include more detailed memory

--- src/main/java/SystemHardwareApp.java
+++ src/main/java/SystemHardwareApp.java
@@ -1,25 +1,37 @@
 import java.io.*;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "system", value = "/system")
 public class SystemHardwareApp extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 
+        int mb = 1024 * 1024;
+        int gb = 1024 * mb;
+
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
         out.println("<html><body>");
         out.println("<h1>System characteristics</h1>");
 
-        out.println("<p>CPU: " + Runtime.getRuntime().availableProcessors() + " cores</p>");
+        out.println("<p>CPU: " + osBean.getAvailableProcessors() + " cores</p>");
 
-        out.println("<p>Total Memory: " + Runtime.getRuntime().totalMemory() / (1024 * 1024) + " MB</p>");
-        out.println("<p>Free Memory: " + Runtime.getRuntime().freeMemory() / (1024 * 1024) + " MB</p>");
-        out.println("<p>Max Memory: " + Runtime.getRuntime().maxMemory() / (1024 * 1024) + " MB</p>");
+        out.println("<p>Total Memory: " + osBean.getTotalMemorySize() / mb + " MB</p>");
+        out.println("<p>Free Memory: " + osBean.getFreeMemorySize() / mb + " MB</p>");
+        out.println("<p>Committed Virtual Memory: " + osBean.getCommittedVirtualMemorySize() / mb + " MB</p>");
 
-        out.println("<p>Disk: " + (new File("/").getTotalSpace() / (1024 * 1024 * 1024)) + " GB</p>");
+        out.println("<p>JVM Max memory: "+ Runtime.getRuntime().maxMemory() / mb + " MB</p>");
+        out.println("<p>JVM Free memory: "+ Runtime.getRuntime().freeMemory() / mb + " MB</p>");
+        out.println("<p>JVM Total memory: "+ Runtime.getRuntime().totalMemory() / mb + " MB</p>");
 
-        out.println("<p>Operating System: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + "</p>");
+        out.println("<p>Disk: " + (new File("/").getTotalSpace() / gb) + " GB</p>");
+
+        out.println("<p>Operating System: " + osBean.getName() + " " + osBean.getVersion() + "</p>");
+        out.println("<p>Architecture: " + osBean.getArch() + "</p>");
         out.println("<p>Java Version: " + System.getProperty("java.version") + "</p>");
 
         out.println("<p>Current Time: " + new java.util.Date() + "</p>");
