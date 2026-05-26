b2082195b52a02129ce052ad4b46b11f4ccf5914
bogdanzmeul <bogdanzmeul@gmail.com>
2026-05-26 23:21:24 +0300

    Collect source code into a JAR file

--- src-code-plugin/src/main/java/src/code/plugin/CodeCollector.java
+++ src-code-plugin/src/main/java/src/code/plugin/CodeCollector.java
@@ -5,88 +5,67 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
 
 @Mojo(name = "collect")
 public class CodeCollector extends AbstractMojo {
-    @Parameter(property = "codeCollector.sourceDirectory", defaultValue = "${project.build.sourceDirectory}")
+
+    @Parameter(property = "codeCollector.sourceDirectory", defaultValue = "${project.basedir}/src")
     private String sourceDirectory;
 
-    @Parameter(property = "codeCollector.outputFile", defaultValue = "${project.build.directory}/collected-sources.java")
+    @Parameter(property = "codeCollector.outputFile", defaultValue = "${project.build.directory}/source-code.jar")
     private String outputFile;
 
     @Override
     public void execute() throws MojoExecutionException {
-        Path sourceDirectoryPath = Path.of(sourceDirectory);
-        Path outputFilePath = Path.of(outputFile);
+        File sourceFolder = new File(sourceDirectory);
+        File jarFile = new File(outputFile);
 
-        if (Files.notExists(sourceDirectoryPath)) {
-            getLog().error("Source directory does not exist: " + sourceDirectoryPath);
-            return;
+        if (!sourceFolder.exists()) {
+            throw new MojoExecutionException("Source folder does not exist: " + sourceFolder.getAbsolutePath());
         }
 
         try {
-            Path outputDirectory = outputFilePath.getParent();
-            if (outputDirectory != null) {
-                Files.createDirectories(outputDirectory);
-            }
-            List<Path> javaFiles = findJavaFiles(sourceDirectoryPath);
-            writeCollectedSources(javaFiles, outputFilePath);
+            jarFile.getParentFile().mkdirs();
 
-            getLog().info("Collected " + javaFiles.size() + " Java source file(s) into " + outputFilePath);
-        } catch (IOException | UncheckedIOException exception) {
-            throw new MojoExecutionException("Failed to collect Java source files", exception);
+            try (JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(jarFile))) {
+                addFolderToJar(sourceFolder, sourceFolder, jarOutputStream);
+            }
+
+            getLog().info("Created source code archive: " + jarFile.getAbsolutePath());
+        } catch (IOException exception) {
+            throw new MojoExecutionException("Failed to create source code archive", exception);
         }
     }
 
-    private List<Path> findJavaFiles(Path sourceDirectoryPath) throws IOException {
-        List<Path> javaFiles = new ArrayList<>();
-        collectJavaFiles(sourceDirectoryPath, javaFiles);
-        Collections.sort(javaFiles);
-        return javaFiles;
-    }
+    private void addFolderToJar(File rootFolder, File currentFolder, JarOutputStream jarOutputStream) throws IOException {
+        File[] files = currentFolder.listFiles();
 
-    private void collectJavaFiles(Path directory, List<Path> javaFiles) throws IOException {
-        try (DirectoryStream<Path> files = Files.newDirectoryStream(directory)) {
-            for (Path file : files) {
-                if (Files.isDirectory(file)) {
-                    collectJavaFiles(file, javaFiles);
-                } else if (Files.isRegularFile(file) && file.toString().endsWith(".java")) {
-                    javaFiles.add(file);
-                }
+        if (files == null) {
+            return;
+        }
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                addFolderToJar(rootFolder, file, jarOutputStream);
+            } else {
+                addFileToJar(rootFolder, file, jarOutputStream);
             }
         }
     }
 
-    private void writeCollectedSources(List<Path> javaFiles, Path outputFilePath) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath.toFile(), StandardCharsets.UTF_8))) {
-            for (Path javaFile : javaFiles) {
-                writer.write("//Filename: " + javaFile);
-                writer.newLine();
+    private void addFileToJar(File rootFolder, File file, JarOutputStream jarOutputStream) throws IOException {
+        String rootPath = rootFolder.getAbsolutePath();
+        String filePath = file.getAbsolutePath();
+        String fileNameInJar = filePath.substring(rootPath.length() + 1).replace("\\", "/");
 
-                try (BufferedReader reader = new BufferedReader(new FileReader(javaFile.toFile(), StandardCharsets.UTF_8))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        writer.write(line);
-                        writer.newLine();
-                    }
-                }
-
-                writer.newLine();
-            }
-        }
+        jarOutputStream.putNextEntry(new JarEntry(fileNameInJar));
+        Files.copy(file.toPath(), jarOutputStream);
+        jarOutputStream.closeEntry();
     }
 }
