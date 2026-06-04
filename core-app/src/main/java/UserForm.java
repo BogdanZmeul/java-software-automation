c1b08a016cfb967bc2916502e6c111f6e5666504
bogdanzmeul <bogdanzmeul@gmail.com>
2026-06-04 22:38:03 +0300

    Fill user form example with validation

--- core-app/src/main/java/UserForm.java
+++ core-app/src/main/java/UserForm.java
@@ -1,5 +1,18 @@
 import compile.GenerateValidation;
+import runtime.*;
 
 @GenerateValidation(className = "ProcessedUserForm")
 public class UserForm {
+    @CheckEmpty
+    String name;
+    @CheckEmail
+    String email;
+    @CheckPhoneNumber
+    String phoneNumber;
+    @CheckPassword
+    String password;
+    @CheckRange(min = 18, max = 100)
+    int age;
+    @CheckRange(min = 1, max = 250)
+    double weight;
 }
