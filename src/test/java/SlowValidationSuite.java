342d1f92f0a40ab2a137bc863cf790d42ff527cb
bogdanzmeul <bogdanzmeul@gmail.com>
2026-06-11 20:33:25 +0300

    Rename FullValidationSuite to SlowValidationSuite

--- src/test/java/SlowValidationSuite.java
+++ src/test/java/SlowValidationSuite.java
@@ -5,5 +5,5 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeTags({"slow", "system"})
 @SelectClasses(UserFormProcessorTest.class)
-public class FullValidationSuite {
+public class SlowValidationSuite {
 }
