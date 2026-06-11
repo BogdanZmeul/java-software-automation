c514ec728e2762214e274841bf7c830bb05ebbf3
bogdanzmeul <bogdanzmeul@gmail.com>
2026-06-11 12:05:22 +0300

    Delete fast tests in FullValidationSuite

--- src/test/java/FullValidationSuite.java
+++ src/test/java/FullValidationSuite.java
@@ -3,7 +3,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@IncludeTags({"fast", "slow", "system"})
+@IncludeTags({"slow", "system"})
 @SelectClasses(UserFormProcessorTest.class)
 public class FullValidationSuite {
 }
