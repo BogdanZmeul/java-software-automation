0dc2fb9889ef89a018de5f88675c6c84c076546d
bogdanzmeul <bogdanzmeul@gmail.com>
2026-06-18 13:29:52 +0300

    Add Mockito and AssertJ dependencies for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.platform:junit-platform-suite")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.23.0")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
