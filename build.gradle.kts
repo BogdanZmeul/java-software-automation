e919a95da51011b9f44a1977c88c50f2bd1dfce4
bogdanzmeul <bogdanzmeul@gmail.com>
2026-06-18 11:02:47 +0300

    Add CI/CD pipeline from homework6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("pmd")
+    `maven-publish`
 }
 
 group = "org.example"
@@ -12,9 +14,39 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-suite")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    reports {
+        junitXml.required.set(false)
+        html.required.set(true)
+    }
+}
+
+pmd {
+    isConsoleOutput = true
+    toolVersion = "7.16.0"
+    ruleSets = listOf("category/java/errorprone.xml")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
